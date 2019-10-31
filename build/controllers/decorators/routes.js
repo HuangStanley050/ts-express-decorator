a8eae8146aeba00022461b1e53c51cebe897994d
Stanley Huang <huang.stanley050@gmail.com>
2019-10-31 20:17:24 +1100

    added enum for all REST methods

--- build/controllers/decorators/routes.js
+++ build/controllers/decorators/routes.js
@@ -1,9 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
-function get(path) {
-    return function (target, key, desc) {
-        Reflect.defineMetadata("path", path, target, key);
+var Methods_1 = require("./Methods");
+function routeBinder(method) {
+    return function (path) {
+        return function (target, key, desc) {
+            Reflect.defineMetadata("path", path, target, key);
+            Reflect.defineMetadata("method", method, target, key);
+        };
     };
 }
-exports.get = get;
+exports.get = routeBinder(Methods_1.Methods.get);
+exports.post = routeBinder(Methods_1.Methods.post);
+exports.put = routeBinder(Methods_1.Methods.put);
+exports.patch = routeBinder(Methods_1.Methods.patch);
+exports.del = routeBinder(Methods_1.Methods.del);
